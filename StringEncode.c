0d9c27d25a860237f659cf14159b601f543e94ba
Lucas Lassila <lassila2@icloud.com>
2023-08-09 10:50:03 -0700

    Add files via upload
    Initial Commit:
    - Outlines project goals

---
+++ StringEncode.c
@@ -0,0 +1,13 @@
+// String Encode C Project
+// Created by Lucas Lassila on 8/9/23.
+// Initial Commit 8/9/23
+// This program is designed to use a string to encode data that will be used to print a picture to console
+
+/* each char in the string will represent a number, The number will determine what action is completed
+    To draw the picture we need to know:
+    1. start a new line?
+    2. What char to print?
+    3. how many of said char to print?
+// The main body of the program could be handled by a for each loop with if else statements to call printing
+
+// Once completed: it could be done in reverse; enter lines and convert them to a string.
