3e0aaa5ca70a23a199a4f2bda0967c5ea7938159
Lucas Lassila <lassila2@icloud.com>
2023-08-09 11:26:17 -0700

    Add files via upload
    This update has a main logic loop with a string, a for loop and some pseudo-code for the printing of the symbols. 
    It also uses C's sizeof() function to find the length of the string. This will be needed as I learned that C does not have a For Each Loop function. 
    
    An integer j is set to be the value of the letter in the string, depending on what the value of j is a series of if else statements will decide what the program will print.

--- StringEncode.c
+++ StringEncode.c
@@ -8,6 +8,36 @@
     1. start a new line?
     2. What char to print?
     3. how many of said char to print?
-// The main body of the program could be handled by a for each loop with if else statements to call printing
+The main body of the program could be handled by a for each loop with if else statements to call printing
 
-// Once completed: it could be done in reverse; enter lines and convert them to a string.
+Once completed: it could be done in reverse; enter lines and convert them to a string.
+ */
+
+#include <stdio.h>
+
+int main{
+    char str[]  = ""; // string here
+
+    int strLength = sizeof(str) / sizeof(str[0]);
+    // string length = mem size of string / mem size of one char
+
+    for(int i = 0; i < stringLength; i++){
+
+        int j = str[i] - 0; //
+
+        if(j == /n){
+            // next line
+        }else if(j == num for star){
+            // print star for val of j++
+            // j++
+        }else if(j == num for space){
+            // print space for val of j++
+            // j++
+        }else{
+            printf("\n Faulty code at position: ", j);
+            break; 
+        }
+
+    }
+};
+
